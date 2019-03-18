e7750928be737f54a395c99a797a0a1a7d60508e
BuildTools <unconfigured@null.spigotmc.org>
2019-03-18 08:16:09 +0100

    V0.1

---
+++ SDIS29_Pertes_de_charges/SDIS29_Pertes_de_charges/www/scripts/index.js
@@ -0,0 +1,18 @@
+﻿function getSdisData() {
+    var choix_lance = localStorage.getItem("choix_lance");
+    var debit_lance = localStorage.getItem("debit_lance");
+    var nb_45 = localStorage.getItem("nb_45");
+    var nb_70 = localStorage.getItem("nb_70");
+    var denivele = localStorage.getItem("denivele");
+    window.alert(debit_lance);
+};
+
+
+function setSdisData(name) {
+    localStorage.setItem(name, document.getElementById(name).value);
+    window.alert(document.getElementById(name).value);
+};
+
+function getPertesDeCharges() {s
+    var nb_45 = localStorage.getItem("nb_45");
+};
